45f6fe8a5760b137969cdf5d252248c14bb3129a
trapcodeio <trapcode.io@gmail.com>
2023-05-24 10:41:13 +0100

    - switch to npm - add docker files

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const serverUrl = import.meta.env.VITE_API_URL;
+const serverUrl = import.meta.env.VITE_API_URL as string | undefined;
+
+if (!serverUrl) {
+  throw new Error("Please define the VITE_API_URL environment variable.");
+}
 
 export const $axios = axios.create({
   baseURL: serverUrl
